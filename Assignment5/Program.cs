da5d01245d1ce884c6d7a9c6441dd3fddf92f748
u31d79 <u31d79@INGURWN110491>
2023-10-31 20:58:55 +0530

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,26 @@
+﻿
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+           // account.GetDetails();
+            Console.WriteLine("Enter account id");
+        account.Id = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter account type");
+            account.AccountType = Console.ReadLine();
+            Console.WriteLine("Enter balance");
+            account.Balance= Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the amount to be withdrawn");
+            double amountWith = Double.Parse(Console.ReadLine());
+            Console.WriteLine(account.GetDetails());
+            if (account.Withdraw(amountWith))
+            {
+                Console.WriteLine($"The new balance after deduction is {account.Balance - amountWith}");
+            }
+
+
+
+        }
+
+    }
